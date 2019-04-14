1d8cd6fe71d628a67d55fc5523425aa9ee743076
AnxQ <xiaoqi990112@hotmail.com>
2019-04-14 12:22:25 +0800

    # Complete the simulation function

--- machine.py
+++ machine.py
@@ -9,9 +9,13 @@ class Status(Enum):
     Move = 1
     Wait = 2
     Work = 3
+    Idle = 0
 
 
 class RGV_queue(list):
+    def __init__(self, *iter):
+        super().__init__(*iter)
+
     def front(self, cur):
         i = self.index(cur)
         return None if i < 0 else self[-1] if i == 0 else self[i - 1]
@@ -28,20 +32,23 @@ def distance(p, r):
 # TODO: Get next RGV destination for the RGV id
 
 class Port:
-    identity: int
+    identity: str
     location: float
-    record_item: queue
+    _I_O: bool
 
-    def __init__(self, identity, location, ):
-        pass
+    def __repr__(self):
+        return f"<ID:{self.identity}, Location:{self.location:.2f}>"
+
+    def __init__(self, identity, location):
+        self.identity = identity
+        self.location = location
+        self._I_O = True if 'I' in self.identity else False
 
     def transport(self, r, tick):
-        self.record_item.append([r.identity, tick])
-
-
-def get_target_by_id(id) -> Port:
-    # 任务分配
-    return 0
+        if self._I_O:
+            print(f"{tick:.2f}: Port:{self.identity} Import into RGV:{r.identity}")
+        else:
+            print(f"{tick:.2f}: Port:{self.identity} Export from RGV:{r.identity}")
 
 
 class RGV:
@@ -49,41 +56,61 @@ class RGV:
     velocity = 1.5
     length = 1.3
     period_work = 10
-    tick_fin: float
-    tick_rec: float
-    stat: Status
+    tick_fin: float = 0
+    tick_rec: float = 0
+    stat: Status = Status.Idle
     location: float
     target: Port
 
-    def update(self, tick_cur, queue: RGV_queue):
+    def __repr__(self):
+        return f"<ID:{self.identity}, Location:{self.location:.2f}, Status:{self.stat.name}, Target:{self.target}>"
+
+    def __init__(self, identity, location):
+        self.identity = identity
+        self.location = location
+
+    def start(self, target):
+        self.target = target
+        self.tick_fin = distance(target, self) / self.velocity
+        self.stat = Status.Move
+
+
+    def update(self, tick_cur, queue: RGV_queue, get_target: Callable):
         """
         将RGV状态刷新到当前时刻，由前向后
         :param tick_cur:
         :param queue:
+        :param get_target:
         :return:
         """
         rgv_front: RGV = queue.front(self)
         if tick_cur == self.tick_fin:
             if self.stat == Status.Work:
                 # 由于本车最先执行完动作导致的时刻刷新
-                self.target = get_target_by_id(self.identity)
+                self.target = get_target(self.identity)
+                if not self.target:
+                    self.stat = Status.Idle
+                    self.tick_fin = rgv_front.tick_fin
+                    return
                 self.stat = Status.Move
-                self.tick_fin = distance(self.target, self) / self.velocity
+                self.tick_fin = tick_cur + distance(self.target, self) / self.velocity
             elif self.stat == Status.Move:
                 # 由于移动到位导致的刷新
                 self.stat = Status.Work
                 self.target.transport(self, tick_cur)
-                self.tick_fin += self.period_work
+                self.tick_fin = tick_cur + self.period_work
             elif self.stat == Status.Wait:
                 # 等待完成导致刷新
                 self.stat = Status.Move
-                self.tick_fin = distance(self.target, self) / self.velocity
+                self.tick_fin = tick_cur + distance(self.target, self) / self.velocity
+            else:
+                self.location = (rgv_front.location - self.length) % 100
             self.tick_rec = tick_cur
             return self.tick_fin
         elif self.stat == Status.Move:
             # 由于前车已完成update，且自己的移动动作此时也未完成，要考虑前方是否进入等待状态
             approach_dis = (tick_cur - self.tick_rec) * self.velocity
-            impact_dis = distance(queue.front(self), self) - self.length
+            impact_dis = distance(rgv_front, self) - self.length
             if impact_dis < approach_dis:
                 # 获取前方预计等待时间 决定自己的tick_fin(此时为等待完成时间) 置入等待状态
                 # 这会导致完成时间提前/延后 因此需要之后以等待完成位置再次计算移动完成时间
@@ -96,8 +123,10 @@ class RGV:
         elif self.stat == Status.Work:
             # 尚在工作,且未完成
             pass
-        else:
+        elif self.stat == Status.Wait:
             # 等待状态
             pass
+        else:
+            self.location = (rgv_front.location - self.length) % 100
         self.tick_rec = tick_cur
         return self.tick_fin
--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from itertools import chain
 import numpy as np
 from queue import Queue
 from random import randint
-from
+from machine import Port, RGV, RGV_queue, Status
 
-work_in_mat = np.delete(np.array(read_xls('B题附件1.xlsx', has_head=True)), 0, axis=1).astype(np.int)
-work_out_mat = np.delete(np.array(read_xls('B题附件2.xlsx', has_head=True)), 0, axis=1).astype(np.int)
+work_in_mat: np.ndarray = np.delete(np.array(read_xls('B题附件1.xlsx', has_head=True)), 0, axis=1).astype(np.int)
+work_out_mat: np.ndarray = np.delete(np.array(read_xls('B题附件2.xlsx', has_head=True)), 0, axis=1).astype(np.int)
+n_rgv = 9
+l_rgv = 1.30
 
 A_queue: List[List] = work_in_mat.tolist()
 B_queue: List[List] = work_out_mat.T.tolist()
@@ -16,11 +18,22 @@ for q in B_queue:
         q.remove(0)
 
 port_loc = [i * 47 / 6 for i in range(1, 6)] + [50 + i * 47 / 9 for i in range(1, 9)]
-Port_queue =
+port_id = ['AO3', 'AO2', 'AO1', 'AI2', 'AI1', 'BO1', 'BI1', 'BO2', 'BI2', 'BO3', 'BI3', 'BO4', 'BI4']
+rgv_loc = [0 - i * l_rgv for i in range(n_rgv)]
+rgv_id = [i for i in range(1, n_rgv + 1)]
+Ports = [Port(i, l + 47 / 2) for i, l in zip(port_id, port_loc)]
+RGVs = RGV_queue([RGV(i, l) for i, l in zip(rgv_id, rgv_loc)])
+
+port_map = [
+    [4, 3],
+    [5, 7, 9, 11],
+    [6, 8, 10, 12],
+    [2, 1, 0]
+]
 
 
 def init_plan():
-    # Example plan
+    # Initial plan
     plan_i_src = list(chain.from_iterable((1, 2) for i in range(100)))
     plan_i_des = list(chain.from_iterable(A_queue))
     plan_o_src = []
@@ -59,14 +72,55 @@ def init_plan():
     return plan
 
 
+def still_running() -> bool:
+    for rgv in RGVs:
+        if rgv.stat is not Status.Idle:
+            return True
+    return False
+
+
+def mutation(len):
+    init_mutation = [randint(0, 1) for i in range(len)]
+    yield init_mutation
+    while True:
+        yield
+
+
 def generation():
     pass
 
 
-def simulate():
-    pass
+def simulate(plan):
+    plan_list = []
+    for j in range(n_rgv):
+        plan_list.append(list(chain.from_iterable(plan[i] for i in range(j, len(plan), n_rgv))))
+    cur_step = [-1 for i in range(n_rgv)]
+
+    print(plan_list[0])
+    print(plan_list[1])
+    print(plan_list[2])
+
+    def allocate_target(id) -> Port:
+        cur_step[id - 1] += 1
+        if cur_step[id - 1] >= len(plan_list[id - 1]):
+            return None
+        while plan_list[id - 1][cur_step[id - 1]] == 0:
+            cur_step[id - 1] += 1
+        idx = cur_step[id - 1]
+        # 由port_map计算出所到转运口index
+        return Ports[port_map[idx % 4][plan_list[id - 1][idx] - 1]]
+
+    for i in range(n_rgv):
+        RGVs[i].start(allocate_target(i+1))
+
+    tick = 0
+    while still_running():
+        tick = min(RGVs, key=lambda x: x.tick_fin).tick_fin
+        for rgv in RGVs:
+            rgv.update(tick, RGVs, allocate_target)
 
 
 if __name__ == "__main__":
-    init_plan()
+    plan = init_plan()
+    simulate(plan)
     pass
